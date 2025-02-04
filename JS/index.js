3d5a68da46426b94d22af287a1c047ca868cced4
Yuleisy C. Rojas <yolocr@yahoo.dk>
2025-02-04 01:31:11 +0100

    Category JS

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+console.log("index script loaded");
+
+fetch(`https://kea-alt-del.dk/t7/api/categories`)
+	.then((response) => response.json())
+	.then(showCategory);
+
+function showCategory(data) {
+	console.log("myCategories:", data);
+
+	const markup = data
+		.map((element) => `<a href="./product_list.html"><button type="button">${element.category}</button></a>`)
+		.join("");
+	console.log("min markup er ", markup);
+	document.querySelector("section").innerHTML = markup;
+}
