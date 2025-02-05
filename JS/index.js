814ea9cc078697bba50c182ed4101ed578155a53
Yuleisy C. Rojas <yolocr@yahoo.dk>
2025-02-05 13:19:47 +0100

    .

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,7 @@
 // console.log("index script loaded");
+// --------------- List of all categories -------------------------------------------
+
+const allCategories = document.querySelector(".categories-container");
 
 fetch(`https://kea-alt-del.dk/t7/api/categories`)
 	.then((response) => response.json())
@@ -8,11 +11,18 @@ function showCategory(data) {
 	console.log("myCategories:", data);
 
 	const markup = data
-		.map((element) => `<a href="./product_list.html"><button type="button">${element.category}</button></a>`)
+		.map(
+			(element) =>
+				`<a href="./product_list.html?categories=${element.category}"><button type="button">${element.category}</button></a>`
+		)
 		.join("");
 	console.log("min markup er ", markup);
-	document.querySelector("section").innerHTML = markup;
+	allCategories.innerHTML = markup;
 }
+
+// --------------- List of all brands -------------------------------------------
+
+const allBrands = document.querySelector(".brands-container");
 
 fetch(`https://kea-alt-del.dk/t7/api/brands`)
 	.then((response) => response.json())
@@ -25,5 +35,5 @@ function showBrands(data) {
 		.map((element) => `<a href="./brands_list.html"><button type="button">${element.brandname}</button></a>`)
 		.join("");
 	console.log("min markup er ", markup);
-	document.querySelector("section").innerHTML = markup;
+	allBrands.innerHTML = markup;
 }
